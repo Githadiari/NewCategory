f3caa8abd8c6044fee0b703c48f663e098f9b5d4
githadiari <githadiari@gmail.com>
2017-12-17 15:31:05 -0700

    Initial commit

---
+++ app/src/main/java/com/example/windows_10/neweis/Interface/ItemClickListener.java
@@ -0,0 +1,12 @@
+package com.example.windows_10.neweis.Interface;
+
+import android.view.View;
+
+/**
+ * Created by windows_10 on 12/17/2017.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongCLick);
+
+}
---
+++ app/src/main/java/com/example/windows_10/neweis/Model/Category.java
@@ -0,0 +1,35 @@
+package com.example.windows_10.neweis.Model;
+
+/**
+ * Created by windows_10 on 12/17/2017.
+ */
+
+public class Category {
+    private String Name;
+    private String Image;
+
+    public Category() {
+    }
+
+    public Category(String name, String image) {
+        Name = name;
+        Image = image;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+}
